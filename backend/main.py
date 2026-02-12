b47f00524301de8ae52ac1f5f8af505eee314e79
thundercloud484-rgb <thundercloud484@gmail.com>
2026-02-13 01:29:17 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://mewmew-1.onrender.com"],  # Allows all origins
+    allow_origins=["https://mewmew-2.onrender.com"],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allows all headers
@@ -54,6 +54,7 @@ def predict_career(data: UserInput):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
+
 
 
 
